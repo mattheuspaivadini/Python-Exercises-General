3f4fbdc4791d5588b950f3f68b4cbe59dc5ff026
Matheus Paiva <matheuspaiva609@gmail.com>
2025-11-12 13:13:51 -0300

    analisedeturma.py added

---
+++ val/analisedeturma.py
@@ -0,0 +1,17 @@
+media_turma = 0
+aprovados = 0
+reprovados = 0
+final = 0
+
+for i in range(1, 6):
+    nota = float(input('Insira a nota do aluno: '))
+    media_turma += nota
+    if nota >= 70 and nota <= 100:
+        aprovados += 1
+    elif nota < 70 and nota >= 40:
+        final += 1
+    elif nota < 40:
+        reprovados += 1
+
+media_final = (media_turma / 5)
+print(f'Aprovados: {aprovados}, final: {final}, reprovados: {reprovados} \n Média da turma: {media_final}')        
